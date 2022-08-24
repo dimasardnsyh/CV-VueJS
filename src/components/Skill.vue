9045c2869deb7d2fe94ce56c463d1a2e7afad4f5
dimasardnsyh <dimasyapemri206@gmail.com>
2022-08-24 07:04:55 +0700

    first commit

---
+++ src/components/Skill.vue
@@ -0,0 +1,67 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import IconHtml from './icons/IconHtml.vue';
+import IconCss from './icons/IconCss.vue';
+import IconBootstrap from './icons/IconBootstrap.vue';
+import IconFigma from './icons/IconFigma.vue';
+
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <IconHtml />
+    </template>
+    <div class="flex">
+        <h3>Html</h3>
+        <h3>85%</h3>
+        </div>
+        <progress max="100" value="85"></progress> 
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <IconCss />
+    </template>
+    <div class="flex">
+        <h3>CSS</h3>
+        <h3>85%</h3>
+        </div>
+        <progress max="100" value="85"></progress>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <IconBootstrap />
+    </template>
+    <div class="flex">
+        <h3>Bootstrap</h3>
+        <h3>80%</h3>
+        </div>
+        <progress max="100" value="80"></progress>
+    
+  </WelcomeItem>
+  <WelcomeItem>
+    <template #icon>
+      <IconFigma />
+    </template>
+    <div class="flex">
+        <h3>Figma</h3>
+        <h3>90%</h3>
+        </div>
+        <progress max="100" value="90"></progress>
+    
+  </WelcomeItem>
+</template>
+
+<style>
+.flex{
+    display: flex;
+    justify-content: space-between;
+}
+
+progress[value] {
+  width: 100%;
+  height: 20px;
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,75 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Profil</template>
+
+    Hallo
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Education</template>
+
+    <h4>2020-2023</h4>
+    <a href="http://portal.smktarunabhakti.net/login/index.php" target="_blank">SMK TARUNA BHAKTI</a>
+    <h4>
+      Web Developer
+    </h4>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Experience</template>
+    <ul>
+      <li>Hotel Design</li>
+      <li>Food Delivery Design</li>
+      <li>Wordpress kartu undangan</li>
+      <li>Wordpress kartu undangan</li>
+    </ul>
+    
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>language</template>
+
+    <ul>
+      <li>English</li>
+      <li>Bahasa</li>
+    </ul>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Contact</template>
+
+    <h4>
+      No tlp: +6285779654993
+    </h4>
+    <h4>
+      Email:dimasyapemri206@gmail.com
+    </h4>
+    <h4>
+      GitHub: <a href="https://github.com/dimasardnsyh" target="_blank">Click To Show</a>
+    </h4>
+  </WelcomeItem>
+</template>
